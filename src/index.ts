db02243d688b1e3939abe28f29df2805ad9b1273
lucasfragadev <lucasfraga.dev@gmail.com>
2025-07-05 22:51:05 -0300

    fix(cors): configure CORS middleware to allow cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ dotenv.config();
 import express from 'express'; // Import Express.
 import routes from './routes'; // Import the main server.
 import connectDB from './config/database';
+import cors from 'cors';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swaggerConfig';
+
 
 const startServer = async () => {
   // Connect to the database and wait for the connection to be established.
@@ -13,9 +18,12 @@ const startServer = async () => {
   const PORT = 3000;
 
   app.use(express.json()); // Middleware to teach Express to read the request body in JSON.
+  app.use(cors());
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   
   app.use(routes); // Tells the application to use the router we imported.
-  
+    
   app.listen(PORT, () => {
     console.log(`The server is running on PORT: ${PORT}!`)
     console.log(`Access directly at: http://localhost:${PORT}`)
