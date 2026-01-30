7bba4069ede90f4a83d378db89193a54331a1ef0
lucasfragadev <lucasfraga.dev@gmail.com>
2026-01-30 14:12:46 -0300

    feat(routes): add family route to index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,15 +4,16 @@ import postRoutes from './postRoutes';
 import userRoutes from './userRoutes';
 import commentRoutes from './commentRoutes';
 import likeRoutes from './likeRoutes';
+import familyRoutes from './familyRoutes';
 
 const router = Router();
 
 // Hub central de rotas da API (v1)
-// Aqui definimos os prefixos para cada módulo. Ex: tudo de user será /users/...
 router.use('/auth', authRoutes); 
 router.use('/posts', postRoutes); 
 router.use('/users', userRoutes); 
 router.use('/comments', commentRoutes); 
 router.use('/likes', likeRoutes);
+router.use('/family', familyRoutes); // 2. E esta linha aqui!
 
 export default router;
